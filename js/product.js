3793a2a30d4d36e395852d983298ed53b2231ac7
Mahmut Aydınalp <150451171+MAHMUTAYDINALP@users.noreply.github.com>
2026-01-16 16:12:14 +0300

    initial  site commit

---
+++ js/product.js
@@ -0,0 +1,17 @@
+fetch('data/product.json')
+ .then(res => res.json())
+ .then(products => {
+    const container= document.getElementById('product=list');
+    
+    products.forEach(p => {
+        container.innerHTML += `
+        <div class="product-card">
+        <img src="${p.image}" alt="${p.name}">
+        <h3>${p.name}</h3>
+        <p>${p.description}</>
+        <span>${p.category}</span>
+        </div>
+
+    `;
+    });
+ }); 
