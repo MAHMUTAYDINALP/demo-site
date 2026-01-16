828a67612142501c694a73d576738b364d8281bd
Mahmut Aydınalp <150451171+MAHMUTAYDINALP@users.noreply.github.com>
2026-01-16 16:36:32 +0300

    site 2 commit

--- js/product.js
+++ js/product.js
@@ -1,17 +1,20 @@
-fetch('data/product.json')
- .then(res => res.json())
- .then(products => {
-    const container= document.getElementById('product=list');
-    
-    products.forEach(p => {
-        container.innerHTML += `
-        <div class="product-card">
-        <img src="${p.image}" alt="${p.name}">
-        <h3>${p.name}</h3>
-        <p>${p.description}</>
-        <span>${p.category}</span>
-        </div>
+fetch("data/product.json")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("product-list");
 
-    `;
+    data.forEach(product => {
+      const card = document.createElement("div");
+      card.className = "product-card";
+
+      card.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h3>${product.name}</h3>
+        <p>${product.description}</p>
+        <span>${product.category}</span>
+      `;
+
+      container.appendChild(card);
     });
- }); 
+  })
+  .catch(err => console.error("JSON okunamadı", err));
